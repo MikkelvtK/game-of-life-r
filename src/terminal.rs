729d0ccbdade75572e7a32f38b0377a8a59dcacb
Michiel van 't Klooster <mvtklooster@hotmail.com>
2023-03-06 23:41:57 +0100

    Start creating DisplayBuilder

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,7 +1,14 @@
+use std::error::Error;
+use std::fmt;
 use std::fmt::Debug;
+use std::io;
 use std::io::Write;
 
+use crossterm::terminal;
+
 use crate::game::Grid;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct Display<T: Write + Debug> {
@@ -10,14 +17,17 @@ pub struct Display<T: Write + Debug> {
     handler: T,
 }
 
+pub struct DisplayBuilder {
+    screen_size: (u16, u16),
+    grid_width: Option<u16>,
+    grid_height: Option<u16>,
+    handler: Box<dyn Write>,
+}
+
 impl<T> Display<T>
 where
     T: Write + Debug,
 {
-    pub fn new() -> Self {
-        unimplemented!()
-    }
-
     pub fn reset_cursor_position(self) -> Self {
         unimplemented!()
     }
@@ -28,5 +38,33 @@ where
 
     pub fn print_grid(self, grid: &Grid) -> Self {
         unimplemented!()
+    }
+}
+
+impl DisplayBuilder {
+    pub fn new() -> MyResult<Self> {
+        let stdout = io::stdout();
+
+        Ok(Self {
+            screen_size: terminal::size()?,
+            grid_width: None,
+            grid_height: None,
+            handler: Box::new(stdout),
+        })
+    }
+
+    pub fn grid_width(self, width: u16) -> Self {
+        unimplemented!()
+    }
+}
+
+impl fmt::Debug for DisplayBuilder {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("DisplayBuilder")
+            .field("screen_size", &self.screen_size)
+            .field("grid_width", &self.grid_width)
+            .field("grid_height", &self.grid_height)
+            .field("handler", &"Box<dyn Write>")
+            .finish()
     }
 }
